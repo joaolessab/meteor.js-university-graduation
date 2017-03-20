b7cec6e2992d880b53cbab10a811d56d5fc90143
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-20 00:18:49 -0300

    Redirecionando as rotas caso não esteja logado

--- Intermediate/lib/routes.js
+++ Intermediate/lib/routes.js
@@ -1,8 +1,19 @@
+//Caso o usuário não esteja logado, ele vai direto para a página HOME
+FlowRouter.triggers.enter([function(context, redirect){
+	if(!Meteor.userId()){
+		FlowRouter.go('home');
+	}
+}]);
+
 FlowRouter.route('/', {
 	name: 'home',
 	action(){
+		//Homepage direcionando para o recipe-book caso o usuário EXISTA
+		if (Meteor.userId()){
+			FlowRouter.go('recipe-book');
+		}
 		// Se você quiser adicionar Google Analytics
-		// GAnalytics.pageview();
+		GAnalytics.pageview();
 		BlazeLayout.render('HomeLayout');
 	}
 });
@@ -11,15 +22,16 @@ FlowRouter.route('/recipe-book', {
 	name: 'recipe-book',
 	action(){
 		// Se você quiser adicionar Google Analytics
-		// GAnalytics.pageview();
+		GAnalytics.pageview();
 		//main é a área principal do MainLayout referenciando o template de Test
 		BlazeLayout.render('MainLayout', {main: 'Recipes'});
 	}
 });
 
 FlowRouter.route('/recipe/:id', {
-	name: 'recipe-book',
+	name: 'recipe',
 	action(){
+		GAnalytics.pageview();
 		BlazeLayout.render('MainLayout', {main: 'RecipeSingle'});
 	}
 });

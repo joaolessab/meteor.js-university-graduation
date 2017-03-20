62e9784107b1cfc4e26571a449821c5ff1ccff9b
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-19 21:05:51 -0300

    Publishing and Subscribe das Collections RECIPE

--- Intermediate/collections/Recipes.js
+++ Intermediate/collections/Recipes.js
@@ -1,6 +1,14 @@
 //Criando a Collection
 Recipes = new Meteor.Collection('recipes');
 
+//Dando autorização para inserção
+Recipes.allow({
+	insert: function(userId, doc){
+		// Se userId existir, então você está logado, então você pode inserir um recipe
+		return !!userId;
+	}
+});
+
 //Criando o Schema
 RecipeSchema = new SimpleSchema({
 	name: {

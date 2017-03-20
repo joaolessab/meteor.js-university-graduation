011fdead1d2c857a158c821a9a663dae8ca92b73
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-19 23:44:48 -0300

    Rotas com Parâmetros (ID, etc)

---
+++ Intermediate/client/recipes/RecipeSingle.js
@@ -0,0 +1,19 @@
+//Recebendo e lendo a Collection do banco, não precisa fazer mais nada
+//Meteor.subscribe('recipes');
+
+//Função criada para limpar e criar de novo, reuso apenas
+Template.RecipeSingle.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes');
+	});
+});
+
+//Criando os helpers para o RecipeSingle
+Template.RecipeSingle.helpers({
+	//Cuidado com o nome da função
+	recipe: () => {
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+});

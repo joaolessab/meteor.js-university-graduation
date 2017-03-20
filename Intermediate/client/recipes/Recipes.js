10c767fa7cc95db56f5d71558b734b3f102bba36
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-19 23:17:58 -0300

    Função modificada das subscriptions

--- Intermediate/client/recipes/Recipes.js
+++ Intermediate/client/recipes/Recipes.js
@@ -1,5 +1,13 @@
 //Recebendo e lendo a Collection do banco, não precisa fazer mais nada
-Meteor.subscribe('recipes');
+//Meteor.subscribe('recipes');
+
+//Função criada para limpar e criar de novo, reuso apenas
+Template.Recipes.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes');
+	});
+});
 
 //Criando os helpers para o Recipes
 Template.Recipes.helpers({

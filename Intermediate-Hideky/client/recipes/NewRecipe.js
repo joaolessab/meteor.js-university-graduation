5730a2bd04e59e0358288178fa9354006d9a07b5
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-21 11:14:48 -0300

    Accounts - Blaze

--- Intermediate-Hideky/client/recipes/NewRecipe.js
+++
@@ -1,5 +0,0 @@
-Template.NewRecipe.events({
-	'click .fa-close': function() {
-		Session.set('newRecipe', false);
-	}
-});
--- Intermediate-Hideky/server/publish.js
+++
@@ -1,8 +0,0 @@
-Meteor.publish('recipes', function() {
-	return Recipes.find({author: this.userId});
-});
-
-Meteor.publish('singleRecipe', function(id) {
-	check(id, String);
-	return Recipes.find({_id: id});
-});

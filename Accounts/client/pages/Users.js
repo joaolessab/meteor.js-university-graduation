7639666074ced88463e9f1f1b345ebe26a1ed609
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-25 14:14:28 -0300

    Visualização de usuários funcionando

--- Accounts/client/pages/Users.js
+++ Accounts/client/pages/Users.js
@@ -3,3 +3,18 @@ Template.Users.onCreated(function(){
 		this.subscribe('allUsers');
 	});
 });
+
+Template.Users.helpers({
+	users: function(){
+		return Meteor.users.find({});
+	},
+	userEmail: function(){
+		//Pegando o primeiro elemento da array
+		return this.emails[0].address;
+	},
+	isAdmin: function(){
+		//Checkando cada linha, tem que ser o _id do mongo
+		//Usando if inline
+		return Roles.userIsInRole(this._id, 'admin') ? 'admin' : 'normal';
+	}
+});

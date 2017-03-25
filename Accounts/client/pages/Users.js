787df3829ece1db3e864f76c35df6c12dbf6e0be
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-25 16:27:29 -0300

    Finalizado modo de edição

--- Accounts/client/pages/Users.js
+++ Accounts/client/pages/Users.js
@@ -24,6 +24,14 @@ Template.Users.helpers({
 	dateFormat: function(){
 		//Função do Moment
 		return moment(this.createdAt).format('D MMMM YYYY hh')
+	},
+	editMode: function(){
+		//Usamos a sessão para verificar se há algo dentro do currentUser clicado, se houver ele habilta a edição, se não, ele nem mostra
+		return Session.get('currentUser') ? 'edit-mode': '';
+	},
+	currentEdit: function(){
+		let user = Session.get('currentUser');
+		return user._id === this._id;
 	}
 });
 
@@ -38,5 +46,8 @@ Template.Users.events({
 		//Chamando o server side Method toogleAdmin e passando o id do usuário atual
 		//Meteor.call('toggleAdmin', this._id);
 		Meteor.call('toggleAdmin', this._id);
+	},
+	'click .close-edit-mode': function(){
+		Session.set('currentUser', null);
 	}
 });

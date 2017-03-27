7a5b253c474e81fc68f39e95f5e09475f480f0f1
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-27 19:57:50 -0300

    Consegui retornar dados

--- TRAPUX/client/pages/MyDashboard.js
+++ TRAPUX/client/pages/MyDashboard.js
@@ -1,7 +1,7 @@
 //Meteor.users.find({"profile.profession": "gestor"}).fetch();
 
 Template.MyDashboard.helpers({
-	kindaOfUser: function(){
+	users: function(){
 		/* Retornando o meu usário apenas */
 		return Meteor.users.find({_id: Meteor.userId()});
 	}

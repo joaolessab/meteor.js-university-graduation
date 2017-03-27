34a6e485b2ff1b8b65e24624e4db971b5aedb5ea
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-27 09:21:39 -0300

    Testando queries com Collections

--- TRAPUX/collections/Copos.js
+++ TRAPUX/collections/Copos.js
@@ -1,4 +1,8 @@
 Copos = new Mongo.Collection('copos');
+//Copos.findOne({ name : "Ronaldo" });
+//Copos.find({}, {fields: {'name': 0}}).fetch()
+//Copos.find({}, {sort: {order: 1}}).fetch()
+//https://docs.meteor.com/api/collections.html
 
 // DANDO AUTORIZAÇÃO PARA MEXER COM O COPOS
 Copos.allow({

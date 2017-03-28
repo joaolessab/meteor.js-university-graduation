59ed62cacb7a4ba9b05aed194a857c4e96238e06
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-27 22:32:02 -0300

    Testando

--- TRAPUX/client/pages/MyDashboard.js
+++ TRAPUX/client/pages/MyDashboard.js
@@ -1,12 +1,20 @@
 //Meteor.users.find({"profile.profession": "gestor"}).fetch();
 
 Template.MyDashboard.helpers({
+	//Chama each users
 	users: function(){
-		/* Retornando o meu usário apenas */
 		return Meteor.users.find({_id: Meteor.userId()});
+	},
+	teste: function(){
+		return 'Essetemplate';
 	}
 });
 
 Template.MyDashboard.events({
+	/*'click .user_id': function(){
+		console.log(this);
+		//Só cria uma sessão com os dados do usuário
+		Session.set('currentUser', this);
 
+	}*/
 });

3e1d5f964b47629770449f86b3d61cd793d8eec1
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-03-28 21:07:41 -0300

    Criando a Collection UXORDERS

--- TRAPUX/collections/Uxorders.js
+++ TRAPUX/collections/Uxorders.js
@@ -1,11 +1,11 @@
-Copos = new Mongo.Collection('copos');
+Uxorders = new Mongo.Collection('uxorders');
 //Copos.findOne({ name : "Ronaldo" });
 //Copos.find({}, {fields: {'name': 0}}).fetch()
 //Copos.find({}, {sort: {order: 1}}).fetch()
 //https://docs.meteor.com/api/collections.html
 
 // DANDO AUTORIZAÇÃO PARA MEXER COM O COPOS
-Copos.allow({
+Uxorders.allow({
 	insert: function(userId, doc){
 		// Se userId existir, então você está logado, então você pode inserir um recipe
 		return !!userId;
@@ -13,4 +13,20 @@ Copos.allow({
 	update: function(userId, doc){
 		return !!userId;
 	}
+});
+
+//Métodos da Collection Uxorders
+Meteor.methods({
+	//Função para update
+	/*toogleMenuItem: function(id, currentState){
+		Uxorders.update(id, {
+			$set: {
+				inMenu: !currentState
+			}
+		})
+	},
+	//Função para remoção
+	deleteRecipe: function(id){
+		Uxorders.remove(id);
+	}*/
 });

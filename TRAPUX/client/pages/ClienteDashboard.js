3ebf0f697b3d8cefc6ce3201556736bd8cbabe8f
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2017-04-02 00:47:14 -0300

    Interface do usuário sendo montada

--- TRAPUX/client/pages/ClienteDashboard.js
+++ TRAPUX/client/pages/ClienteDashboard.js
@@ -6,5 +6,28 @@ Template.ClienteDashboard.helpers({
 });
 
 Template.ClienteDashboard.events({
-	
+	'click #create': ()=> {
+		$(".itemBtn").removeClass("actionSelected");
+		$("#create").addClass("actionSelected");
+
+		$(".panels").hide();
+	},
+	'click #view': ()=> {
+		$(".itemBtn").removeClass("actionSelected");
+		$("#view").addClass("actionSelected");
+
+		$(".panels").hide();
+	},
+	'click #evaluate': ()=> {
+		$(".itemBtn").removeClass("actionSelected");
+		$("#evaluate").addClass("actionSelected");
+
+		$(".panels").hide();
+	},
+	'click #chart': ()=> {
+		$(".itemBtn").removeClass("actionSelected");
+		$("#chart").addClass("actionSelected");
+
+		$(".panels").hide();
+	}	
 });
